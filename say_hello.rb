24bcaf5532e786e95dcba753065dc0f9940cf043
rachel1032 <racheles32@gmail.com>
2018-07-10 15:39:21 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -4,11 +4,12 @@ your_name = ("Nimisha")
 my_name = ("Rachel")
 
 
-def say_hello (your_name, my_name)
+def say_hello1 (your_name, my_name)
   puts "Hello #{your_name}! I'm #{my_name}"
 end
 
-say_hello("Nimisha","Rachel")
+say_hello1("Nimisha","Rachel")
 
-say_hello (ruby_programmer)
+def say_hello (name = "Ruby Programmer")
+  
   
